688f0910b790ea7a39b75ba025bd7c4df993a580
Jigar <chavada.jigar.3@gmail.com>
2019-03-31 00:21:12 +0530

    added files

--- playground/vision_api.py
+++ playground/vision_api.py
@@ -4,34 +4,50 @@ from google.oauth2 import service_account
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
+
 credentials = service_account.Credentials. from_service_account_file('../assets/walmart.json')
+
 
 client = vision.ImageAnnotatorClient(credentials=credentials)
 
-
-# Instantiates a client
-# client = vision.ImageAnnotatorClient()
-
-# The name of the image file to annotate
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    './image2.png')
+file_name = "../assets/all_data/doctors_name.jpg"
 
 # Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
     content = image_file.read()
 
-image = types.Image(content=content)
-
-# Performs label detection on the image file
+# try:
+image = vision.types.Image(content=content)
 response = client.text_detection(image=image)
 texts = response.text_annotations
-print('Texts:')
 
-for text in texts:
-    print('\n"{}"'.format(text.description))
+# print('Texts:')
 
-    # vertices = (['({},{})'.format(vertex.x, vertex.y)
-    #             for vertex in text.bounding_poly.vertices])
+print(texts[0].description.strip())
 
-    # print('bounds: {}'.format(','.join(vertices)))
+# except:
+#     print("kat gaya")
+# import requests
+
+# subscription_key = "d7dcc3f8dbbd44dea57f3ecfb039363e"
+# assert subscription_key
+
+# vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
+
+# text_recognition_url = vision_base_url + "read/core/asyncBatchAnalyze"
+
+# # Set image_url to the URL of an image that you want to analyze.
+# image_path = "/home/jigar/walmart-labs/pharmazon-backend/playground/image.png"
+# image_data = open(image_path, "rb").read()
+
+# headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+# # Note: The request parameter changed for APIv2.
+# # For APIv1, it is 'handwriting': 'true'.
+# params = {'mode': 'Handwritten'}
+# data = {'url': image_data}
+# response = requests.post(
+#     text_recognition_url, headers=headers, params=params, json=data)
+# # response.raise_for_status()
+
+# print(dir(response))
+# print(response.text)
--- src/app.py
+++ src/app.py
@@ -2,9 +2,14 @@ from flask import Flask, jsonify, request
 from bs4 import BeautifulSoup
 import requests
 import os
+import io
 from pymongo import MongoClient
 from bson import json_util, ObjectId
 import json
+from PIL import Image
+from google.oauth2 import service_account
+from google.cloud import vision
+from google.cloud.vision import types
 
 app = Flask(__name__)
 
@@ -18,6 +23,10 @@ db = client['walmart-labs']
 cart = db['card']
 history = db['history']
 
+# Cloud Vision
+credentials = service_account.Credentials. from_service_account_file('../assets/walmart.json')
+client = vision.ImageAnnotatorClient(credentials=credentials)
+
 
 @app.route('/medicine/<query>/', methods=['GET'])
 def getMedicineList(query):
@@ -26,7 +35,7 @@ def getMedicineList(query):
 
     meds = soup.select(".wbrk")
     price = soup.select(".cursor td:nth-of-type(3)")
-    
+
     count = -1
     medList = []
     for med in meds:
@@ -111,19 +120,105 @@ def getCart():
     for c in cart.find():
         arr.append(json.loads(json_util.dumps(c)))
 
-    return jsonify({'data': arr})
+    arr2 = []
+    for c in history.find():
+        arr2.append(json.loads(json_util.dumps(c)))
+
+    return jsonify({'data': arr, 'data2': arr2})
 
 
 @app.route("/user/cart", methods=['POST'])
 def postCart():
-    # data = {
-    #     "hello": "hello"
-    # }
+    data = {
+        "hello": "hello"
+    }
 
-    data = request.json['']
+    # data = request.json['']
     cart.insert_one(data)
 
     return "Done Inserting"
+
+
+@app.route("/user/cart", methods=['DELETE'])
+def buy():
+    arr = []
+    for c in cart.find():
+        # print(json.loads(json_util.dumps(c)))
+        data_dict = json.loads(json_util.dumps(c))
+        del data_dict["_id"]
+        print(data_dict)
+        history.insert_one(data_dict)
+
+    # print(arr)
+    cart.drop()
+    # history.insert_many(arr)
+
+    return "Done Bro"
+
+
+@app.route("/prescription/upload", methods=['POST'])
+def uploadPrescription():
+    file = request.files['file']
+    file.save(os.path.join('../assets/uploaded_prescription/', file.filename))
+    imageObject = Image.open("../assets/uploaded_prescription/" + file.filename)
+
+    data_to_return = {}
+
+    doctors_name = imageObject.crop((10, 100, 1000, 500))
+    registration_number = imageObject.crop((1100, 150, 1900, 300))
+    email = imageObject.crop((1500, 410, 2150, 500))
+    doctors_sig = imageObject.crop((10, 2700, 1000, 3000))
+    stamp = imageObject.crop((1100, 2000, 1900, 2500))
+
+    prsp1 = imageObject.crop((100, 910, 1400, 1170))
+    prsp2 = imageObject.crop((100, 1170, 1400, 1410))
+    prsp3 = imageObject.crop((100, 1410, 1400, 1620))
+    prsp4 = imageObject.crop((100, 1620, 1400, 1830))
+    prsp5 = imageObject.crop((100, 1830, 1400, 2070))
+
+    dosage1 = imageObject.crop((1400, 910, 2100, 1170))
+    dosage2 = imageObject.crop((1400, 1170, 2100, 1410))
+    dosage3 = imageObject.crop((1400, 1410, 2100, 1620))
+    dosage4 = imageObject.crop((1400, 1620, 2100, 1830))
+    dosage5 = imageObject.crop((1400, 1830, 2100, 2070))
+
+    doctors_name.save("../assets/all_data/doctors_name.jpg")
+    registration_number.save("../assets/all_data/registration_number.jpg")
+    email.save("../assets/all_data/email.jpg")
+    doctors_sig.save("../assets/all_data/doctors_sig.jpg")
+    stamp.save("../assets/all_data/stamp.jpg")
+
+    prsp1.save("../assets/all_data/prsp1.jpg")
+    prsp2.save("../assets/all_data/prsp2.jpg")
+    prsp3.save("../assets/all_data/prsp3.jpg")
+    prsp4.save("../assets/all_data/prsp4.jpg")
+    prsp5.save("../assets/all_data/prsp5.jpg")
+
+    dosage1.save("../assets/all_data/dosage1.jpg")
+    dosage2.save("../assets/all_data/dosage2.jpg")
+    dosage3.save("../assets/all_data/dosage3.jpg")
+    dosage4.save("../assets/all_data/dosage4.jpg")
+    dosage5.save("../assets/all_data/dosage5.jpg")
+
+    names_arr = ["doctors_name.jpg", "registration_number.jpg", "email.jpg", "doctors_sig.jpg", "stamp.jpg",
+                 "prsp1.jpg", "prsp2.jpg", "prsp3.jpg", "prsp4.jpg", "prsp5.jpg", "dosage1.jpg", "dosage2.jpg",
+                 "dosage3.jpg", "dosage4.jpg", "dosage5.jpg"]
+
+    for name in names_arr:
+        file_name = "../assets/all_data/" + name
+        with io.open(file_name, 'rb') as image_file:
+            content = image_file.read()
+
+        image = types.Image(content=content)
+        response = client.text_detection(image=image)
+        texts = response.text_annotations
+
+        if(len(texts) != 0):
+            print(name, texts[0].description)
+        else:
+            print(name, "")
+
+    return "Uploaded File"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
